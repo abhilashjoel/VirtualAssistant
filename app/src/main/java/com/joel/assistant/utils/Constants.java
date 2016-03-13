f359fe39c27c32619d43448a336bbd863dbf787e
Abhilash Joel <abhilashjoel17@gmail.com>
2016-03-13 15:58:14 +0530

    Create Constants.java

---
+++ app/src/main/java/com/joel/assistant/utils/Constants.java
@@ -0,0 +1,23 @@
+package com.joel.assistant.utils;
+
+/**
+ * Created by Joel on 2/13/2016.
+ */
+public class Constants {
+
+    public static String Request = "FRAGMENT_REQUEST";
+    public static String Response = "FRAGMENT_RESPONSE";
+    public static String Response_Text = "RESPONSE_TEXT";
+    public static String Response_Web = "RESPONSE_WEB";
+
+
+    /*---------------------------      API.AI Params      --------------------------------*/
+    public static String AI_SCHEME = "https";
+    public static String AI_HOST = "api.api.ai";
+    public static String AI_URL_SEG = "/api/query";
+    public static String AI_AUTH = "Bearer " + "YOUR - AUTHENTICATION - KEY - GOES - HERE";
+    public static String AI_SUBSCR_KEY = "YOUR - SUBSCRIPTION KEY GOES HERE";
+/*------------------------------------------------------------------------------------*/
+
+
+}
